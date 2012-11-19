2f26d50a7c6cf5e6f8c8dcb3a6d5d8dd367d1ff9
Brian Picciano <bgp2396@bellsouth.net>
2012-11-18 21:55:13 -0500

    added comments for previous commit

--- src/nfs.erl
+++ src/nfs.erl
@@ -20,20 +20,25 @@ add_whole_file(FileName) ->
         success
     end.
 
+%Returns if a file exists here (bool)
 exists_locally(FileName) ->
     ndb:file_exists(FileName).
 
+%Returns if a file exists on another given node (bool)
 exists_on_node(Node,FileName) ->
     rpc:call(Node,?MODULE,exists_locally,[FileName]).
 
+%Returns a list of all the nodes a file exists on
 exists_on_which_nodes(FileName) -> ?MODULE:exists_on_which_nodes(FileName,nutil:nnodes()).
 exists_on_which_nodes(FileName,Nodes) ->
     nutil:filter_map(fun(Node) ->
          ?MODULE:exists_on_node(Node,FileName) andalso Node
     end,Nodes).
 
+%Returns a list of all the nodes (except this one) a file exists on
 exists_on_which_other_nodes(FileName) -> 
     ?MODULE:exists_on_which_nodes(FileName,nutil:nnodes_sans_me()).
 
+%Reserves a file and returns the reserved file's name (binary)
 reserve_file() ->
     ndb:reserve_file().
